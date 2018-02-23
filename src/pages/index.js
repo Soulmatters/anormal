d03be2b7b8f19f8f9544959c2fcefbdbd0d3538f
Soulmatters <paul@soulmatters.ro>
2018-02-23 14:40:25 +0200

    styles new

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from 'gatsby-link'
 import graphql from 'graphql'
 import Image from 'gatsby-image'
-import '/style.css'
+import './style.css'
 
 import {
   Card,
